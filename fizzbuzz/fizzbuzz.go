647a5d91351b20189dde276a3617f7e91f8b4fdb
Graeme Boy <graemeboy@gmail.com>
2015-02-15 10:44:50 -0800

    neaten code a little

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -20,29 +20,33 @@ func main() {
     for scanner.Scan() {
         //'scanner.Text()' represents the test case, do something with it
         input := scanner.Text()
-        output := ""
         inArr := strings.Split(input, " ")
-        n, err := strconv.ParseInt(inArr[2], 10, 64)
-        y, err := strconv.ParseInt(inArr[1], 10, 64)
-        x, err := strconv.ParseInt(inArr[0], 10, 64)
-        if (err != nil) {
+        n, err := strconv.Atoi(inArr[2])
+        y, err1 := strconv.Atoi(inArr[1])
+        x, err2 := strconv.Atoi(inArr[0])
+        if (err != nil || err1 != nil || err2 != nil) {
             fmt.Printf("Found error.")
         } else {
-            for i := 1; i <= int(n); i++ {
-                output = ""
-                if ( i % int(x) == 0) {
-                    output += "F"
-                }
-                if (i % int(y) == 0) {
-                    output += "B"
-                } 
-
-                if (output == "") {
-                    output += strconv.Itoa(i)
-                }
-                fmt.Printf(output + " ")
-            }
-            fmt.Printf("\n")
+            do_fb(n, x, y)
         }
-    }   
+    }
+}
+
+func do_fb(n int, x int, y int) {
+    output := ""
+    for i := 1; i <= int(n); i++ {
+        output = ""
+        if ( i % int(x) == 0) {
+            output += "F"
+        }
+        if (i % int(y) == 0) {
+            output += "B"
+        } 
+
+        if (output == "") {
+            output += strconv.Itoa(i)
+        }
+        fmt.Print(output + " ")
+    }
+    fmt.Println()
 }
